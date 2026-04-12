7d37d9430f1b6b4b07173e08f42f734a08cc9355
Qtzl_ <107888322+quetzalcoat1@users.noreply.github.com>
2026-04-13 01:46:13 +0200

    Added handler for account creation

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 
+
+
+
+
 let supabaseClient;
 
-let bouton_ajouter_offre_emploi;
-let liste_offre_emploi;
-let liste_mes_offres;
-let template_offre;
 
+//données utilisateur
 let prenom_utilisateur;
 let nom_utilisateur;
 
 let id_utilisateur = 2;
+
+
+//UI :
+
+//listes
+let liste_offre_emploi;
+let liste_mes_offres;
+let template_offre;
+
+//boutons
+let bouton_ajouter_offre_emploi;
 let bouton_changer_id_utilisateur;
 
-let menu = 0;
+//menus
+let current_menu_id = 0;
 let menu_button_list;
 let menu_list = [];
+
+
+
+
+
+
+
+
+
+//INITIALISATION
+
 
 window.onload = () => {
     
@@ -28,22 +52,7 @@ window.onload = () => {
 
     bouton_changer_id_utilisateur = document.getElementById('button_id_utilisateur');
 
-    bouton_ajouter_offre_emploi.addEventListener('click', async () => {
-        
-        const nom_offre = document.getElementById('input_nom_offre').value;
-        const nom_employeur = document.getElementById('input_nom_employeur').value;
-
-        const { data, error } = await supabaseClient
-            .from('offre_emploi')
-            .insert([{ nom: nom_offre, id_employeur: nom_employeur, id_createur: id_utilisateur }]);
-
-        if (error) {
-            console.error(error);
-        } else {
-            console.log("Offre ajoutée :", data);
-            get_mes_offres(); // afficher directement
-        }
-    });
+    bouton_ajouter_offre_emploi.addEventListener('click', async () => handle_bouton_ajouter_offre_emploi());
 
     bouton_changer_id_utilisateur.addEventListener('click', async () => {
         id_utilisateur = parseInt(document.getElementById('input_id_utilisateur').value);
@@ -62,19 +71,18 @@ window.onload = () => {
 
     menu_button_list = document.getElementById('navbar').children;
     for (let i = 0; i < menu_button_list.length; i++) {
-        button = menu_button_list[i];
+        let button = menu_button_list[i];
         let button_menu_id = button.getAttribute("menu_id");
         button.addEventListener('click', async () => {
-            menu = button_menu_id;
-            display_menu(menu);
+            display_menu(button_menu_id);
         });
     }
 
     display_menu(0);
     
-    // Récupère ton URL et ta clé publique (anon key) depuis Supabase Dashboard
-    const SUPABASE_URL = "https://yxyzcmzjezaechwirlau.supabase.co";   // ton URL de projet
-    const SUPABASE_ANON_KEY = "sb_publishable_mrOPsTPNmHR9jwqUcVrJ-Q_pq-y95sI";               // ta clé publique
+    // URL et clé publique (anon key) depuis Supabase Dashboard
+    const SUPABASE_URL = "https://yxyzcmzjezaechwirlau.supabase.co";                // URL du projet
+    const SUPABASE_ANON_KEY = "sb_publishable_mrOPsTPNmHR9jwqUcVrJ-Q_pq-y95sI";     // clé publique
 
     // Crée le client Supabase
     supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
@@ -82,10 +90,106 @@ window.onload = () => {
 
     get_all();
 
+
+
+
+
+
+
+
+
+
+
+
+    const cards = document.querySelectorAll('.card');
+    const button = document.getElementById('bouton_creer_compte');
+    const input_entreprise = document.getElementById('input_entreprise');
+    console.log(button);
+
+    let selectedRole = null;
+
+    cards.forEach(card => {
+    card.addEventListener('click', () => {
+        // reset selection
+        cards.forEach(c => c.classList.remove('selected'));
+
+        // select clicked
+        card.classList.add('selected');
+        selectedRole = card.dataset.role;
+
+        // show input entreprise if employeur
+        if (selectedRole == 'employeur') {
+            input_entreprise.classList.remove('hidden');
+        } else {
+            input_entreprise.classList.add('hidden');
+        }
+        console.log("Rôle choisi :", selectedRole);
+        // enable button
+        button.disabled = false;
+    });
+    });
+
+    button.addEventListener('click', handle_bouton_creer_compte);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
+
+function get_all() {
+    get_nom_utilisateur();
+    get_offre_emploi();
+    get_mes_offres();
+    get_photo_profil();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MENU
 
 function display_menu(menu_id) {
     
+    current_menu_id = menu_id;
     //console.log(menu_list);
     for (let i = 0; i < menu_list.length; i++) {
         if (menu_list[i] != null) {
@@ -103,12 +207,22 @@ function display_menu(menu_id) {
     }
 }
 
-function get_all() {
-    get_nom_utilisateur();
-    get_offre_emploi();
-    get_mes_offres();
-    get_photo_profil();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DB REQUESTS
 
 async function get_photo_profil() {
 
@@ -117,6 +231,9 @@ async function get_photo_profil() {
         .select('url_photo_profil')
         .eq('id_utilisateur', id_utilisateur)
         .single();
+    if (data == null) {
+        throw new Error("Impossible de récupérer l'url de la photo de profil de l'utilisateur");
+    }
     let local_photo_url = data.url_photo_profil;
 
     if(local_photo_url) {
@@ -124,7 +241,9 @@ async function get_photo_profil() {
         const { data: publicData, error: publicError } = supabaseClient.storage
         .from('photo_profil')
         .getPublicUrl(local_photo_url);
-
+        if (publicData == null) {
+            throw new Error("Impossible de récupérer la photo de profil de l'utilisateur");
+        }
         let public_photo_url = publicData.publicUrl;
         refresh_photo_profil(public_photo_url);
     }
@@ -133,24 +252,14 @@ async function get_photo_profil() {
     }
 }
 
-async function refresh_photo_profil(public_photo_url) {
-    if (public_photo_url) {
-        document.getElementById('photo_profil').src = public_photo_url;
-    }
-    else {
-        document.getElementById('photo_profil').src = "";
-    }
-}
-
 /*
-INPUT l image
+pour input une image :
 
 const { data, error } = await supabase
 .storage
 .from('photo_profil')
-.upload('professional-young-man-stockcake.webp', file); // file = File object depuis input */
-
-
+.upload('professional-young-man-stockcake.webp', file); // file = File object depuis input
+*/
 
 
 
@@ -160,19 +269,29 @@ async function get_nom_utilisateur() {
     .select('*')
     .eq('id_utilisateur', id_utilisateur);
 
+    if (data == null) {
+        throw new Error("Impossible de récupérer le nom de l'utilisateur");
+    }
+
     refresh_nom_utilisateur(data[0].nom, data[0].prenom);
 }
 
-function refresh_nom_utilisateur(nom, prenom) {
-    prenom_utilisateur.textContent = `${prenom}`;
-    nom_utilisateur.textContent = `${nom}`;
-}
 
 async function get_mes_offres() {
     const { data, error } = await supabaseClient
     .from('offre_emploi')
-    .select('*,employeur!inner(nom) as nom_employeur')
-    .eq('id_createur', id_utilisateur);
+    .select(`
+    *,
+    employeur!inner(
+        id_employeur,
+        entreprise!inner(nom_entreprise)
+    )
+    `)
+    .eq('id_employeur', id_utilisateur);
+
+    if (data == null) {
+        throw new Error("Impossible de récupérer les offres de l'utilisateur");
+    }
 
     refresh_liste_mes_offres(data);
 }
@@ -180,46 +299,37 @@ async function get_mes_offres() {
 async function get_offre_emploi() {
     const { data, error } = await supabaseClient
     .from('offre_emploi')
-    .select('*,employeur!inner(nom) as nom_employeur')
-    .neq('id_createur', id_utilisateur);
+    .select(`
+    *,
+    employeur!inner(
+        id_employeur,
+        entreprise!inner(nom_entreprise)
+    )
+    `)
+    .neq('id_employeur', id_utilisateur);
 
-    //console.log(data);
-    //console.log(error);
+    if (data == null) {
+        throw new Error("Impossible de récupérer les offres générales");
+    }
+
     refresh_liste_offre_emploi(data);
 }
 
-function refresh_liste_offre_emploi(data) {
-    liste_offre_emploi.textContent = '';
-    data.forEach(offre_emploi => {
-        add_item_offre_emploi_to(liste_offre_emploi, offre_emploi.employeur.nom, offre_emploi.nom, offre_emploi.id_offre_emploi);
-    });
-}
+async function add_offre_emploi(nom_offre, refresh=false)
+{
+    const { data, error } = await supabaseClient
+        .from('offre_emploi')
+        .insert([{ nom_offre: nom_offre, id_employeur: id_utilisateur }]);
 
-function refresh_liste_mes_offres(data) {
-    liste_mes_offres.textContent = '';
-    data.forEach(offre_emploi => {
-        add_item_offre_emploi_to(liste_mes_offres, offre_emploi.employeur.nom, offre_emploi.nom, offre_emploi.id_offre_emploi, true);
-    });
-}
-
-function add_item_offre_emploi_to(liste, entreprise = "", nom, id_offre_emploi, son_offre = false) {
-
-    const clone_offre = template_offre.cloneNode(true);
-    clone_offre.id = '';
-    clone_offre.childNodes[1].childNodes[1].textContent = `${nom}`;
-    clone_offre.childNodes[1].childNodes[3].textContent = `${entreprise}`;
-    clone_offre.setAttribute("id_offre_emploi", id_offre_emploi);
-
-    if (son_offre) {
-        clone_offre.childNodes[3].classList.remove('hidden');
-        
-        clone_offre.childNodes[3].addEventListener('click', async () => {
-            delete_offre_emploi(id_offre_emploi);
-        });
+    if (error) {
+        console.error(error);
+    } else {
+        console.log("Offre ajoutée :", nom_offre, ":", data);
     }
-
-    liste.appendChild(clone_offre);
-};
+    if (refresh) {
+        get_mes_offres(); // afficher directement
+    }
+}
 
 async function delete_offre_emploi(id_offre_emploi) {
 
@@ -233,4 +343,161 @@ async function delete_offre_emploi(id_offre_emploi) {
 
 
 
-//🗑️
+
+
+
+
+
+
+
+
+
+
+
+
+
+//REFRESH UI
+
+function refresh_photo_profil(public_photo_url) {
+    if (public_photo_url) {
+        document.getElementById('photo_profil').src = public_photo_url;
+    }
+    else {
+        document.getElementById('photo_profil').src = "";
+    }
+}
+
+function refresh_nom_utilisateur(nom, prenom) {
+    prenom_utilisateur.textContent = `${prenom}`;
+    nom_utilisateur.textContent = `${nom}`;
+}
+
+function refresh_liste_offre_emploi(data) {
+    liste_offre_emploi.textContent = '';
+    data.forEach(offre_emploi => {
+        add_item_offre_emploi_to(liste_offre_emploi, offre_emploi.employeur.entreprise.nom_entreprise, offre_emploi.nom_offre, offre_emploi.id_offre_emploi);
+    });
+}
+
+function refresh_liste_mes_offres(data) {
+    liste_mes_offres.textContent = '';
+    data.forEach(offre_emploi => {
+        add_item_offre_emploi_to(liste_mes_offres, offre_emploi.employeur.entreprise.nom_entreprise, offre_emploi.nom_offre, offre_emploi.id_offre_emploi, true);
+    });
+}
+
+function add_item_offre_emploi_to(liste, entreprise = "", nom, id_offre_emploi, mon_offre = false) {
+
+    const clone_offre = template_offre.cloneNode(true);
+    clone_offre.id = '';
+    clone_offre.childNodes[1].childNodes[1].textContent = `${nom}`;
+    clone_offre.childNodes[1].childNodes[3].textContent = `${entreprise}`;
+    clone_offre.setAttribute("id_offre_emploi", id_offre_emploi);
+
+    if (mon_offre) {
+        clone_offre.childNodes[3].classList.remove('hidden');
+        
+        clone_offre.childNodes[3].addEventListener('click', async () => {
+            delete_offre_emploi(id_offre_emploi);
+        });
+    }
+
+    liste.appendChild(clone_offre);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BUTTON HANDLERS
+
+function handle_bouton_ajouter_offre_emploi () {
+
+    const nom_offre = document.getElementById('input_nom_offre').value;
+    //const nom_employeur = document.getElementById('input_nom_employeur').value; plus besoin dcp ? car chaque employeur a une seule entreprise
+    add_offre_emploi(nom_offre, refresh=true);
+}
+
+
+async function handle_bouton_creer_compte() {
+
+    const prenom = document.getElementById('input_prenom_utilisateur').value;
+    const nom = document.getElementById('input_nom_utilisateur').value;
+    const email = document.getElementById('input_email_utilisateur').value;
+
+    const selectedCard = document.querySelector('.card.selected');
+    const role = selectedCard ? selectedCard.dataset.role : null;
+
+    if (!prenom || !nom || !email || !role) {
+        console.log("Champs manquants");
+        return;
+    }
+    if (role == "employeur") {
+        const entreprise = document.getElementById('input_entreprise').value;
+        if (!entreprise) {
+            console.log("Entreprise manquante");
+            return;
+        }
+    }
+
+
+/*
+    if (role == "employeur") {
+        //
+    else if (role == "chercheur") {
+
+    }
+*/
+
+    const { data, error } = await supabaseClient
+        .from('utilisateur')
+        .insert([
+            {
+                prenom: prenom,
+                nom: nom,
+                email: email,
+                type: role,
+            }
+        ])
+        .select();
+
+    if (error) {
+        console.error("Erreur création compte :", error);
+        return;
+    }
+
+    console.log("Compte créé :", data);
+
+    // optionnel : récupérer id utilisateur
+    console.log("ID du nouvel utilisateur :", data[0].id_utilisateur);
+    id_utilisateur = data[0].id_utilisateur;
+
+    display_menu(0);
+    get_all();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
